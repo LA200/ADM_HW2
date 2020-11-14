304d8788ba8a38557cb538e94dceba153a160223
Nina Zorina <zorina@qi4m.com>
2020-11-14 15:08:24 +0300

    Code polishing and big merging

--- RQ2.sh
+++ RQ2.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-files=`ls datasets -ls | grep -v '^total' | awk '{print $10}'`
+# find all csv files in datasets folder except ones containing '.rg' pattern
+files=`ls datasets -l | grep -v '\.rq' | grep 'csv' | awk '{print $9}'`
 for file_base in $files
 do
     echo $file_base
     dataset_file_path="datasets/$file_base"
-    
+
     # Subset with purchases only
-    sub_dataset_with_purchases_file_path="purchases/$file_base.purchases.csv"
+    sub_dataset_with_purchases_file_path="datasets/$file_base.purchases.rq2.csv"
     # Grab only the purchase events, save categories
     # head -n 1 $dataset_file_path > $sub_dataset_with_purchases_file_path
     cat $dataset_file_path | cut -f2,5 -d , | grep purchase | cut -f2 -d , > $sub_dataset_with_purchases_file_path
 
     # Identify unique categories
-    list_of_purchases_categories_file_path="purchases/$file_base.purchases-categories.csv"
+    list_of_purchases_categories_file_path="datasets/$file_base.purchases-categories.rq2.csv"
     cat $sub_dataset_with_purchases_file_path | sort | uniq > $list_of_purchases_categories_file_path
 
     # Generate Sales per category
     all_purchases_categories=`cat $list_of_purchases_categories_file_path`
-    aggregated_n_purchases_per_category_file_path="purchases/$file_base.purchases-per-category.csv"
+    aggregated_n_purchases_per_category_file_path="datasets/$file_base.rq2.csv"
     echo "category,n_purchases" > $aggregated_n_purchases_per_category_file_path
     for purchases_category in $all_purchases_categories
     do
@@ -26,5 +27,4 @@ do
         echo -n ','  >> $aggregated_n_purchases_per_category_file_path
         cat $sub_dataset_with_purchases_file_path | grep "^$purchases_category$" | wc -l  >> $aggregated_n_purchases_per_category_file_path
     done
-
 done
---
+++ RQ6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# find all csv files in datasets folder except ones containing '.rg' pattern
+files=`ls datasets -l | grep -v '\.rq' | grep 'csv' | awk '{print $9}'`
+for file_base in $files
+do
+  echo $file_base
+  dataset_file_path="datasets/$file_base"
+
+  # Subset with purchases only
+  sub_dataset_with_columns_file_path="datasets/$file_base.rq6.csv"
+  # Grab only the columns we need
+  # head -n 50 $dataset_file_path | cut -f2-3,5,8 -d , > $sub_dataset_with_columns_file_path
+  cat $dataset_file_path | cut -f2-3,5,8 -d , > $sub_dataset_with_columns_file_path
+done
